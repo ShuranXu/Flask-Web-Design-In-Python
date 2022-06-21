09ce7d1dcf8f458f5c5454b77d6386e9e2be4568
ShuranXu <shuran.xu@mail.utoronto.ca>
2022-06-21 00:13:45 -0400

    Adapted changes made in jwt_extended to make the app run

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_restful import Api
 from flask_jwt_extended import JWTManager
 
 from db import db
-from blacklist import BLACKLIST
+from blocklist import BLACKLIST
 from resources.user import UserRegister, UserLogin, User, TokenRefresh, UserLogout
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
@@ -20,8 +20,8 @@ JWT related configuration. The following functions include:
 2) customize the token expired error message 
 """
 app.config['JWT_SECRET_KEY'] = 'jose'  # we can also use app.secret like before, Flask-JWT-Extended can recognize both
-app.config['JWT_BLACKLIST_ENABLED'] = True  # enable blacklist feature
-app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']  # allow blacklisting for access and refresh tokens
+# app.config['JWT_BLACKLIST_ENABLED'] = True  # enable blacklist feature
+# app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']  # allow blacklisting for access and refresh tokens
 jwt = JWTManager(app)
 
 """
@@ -29,7 +29,7 @@ jwt = JWTManager(app)
 and for each jwt protected endpoint, we can retrieve these claims via `get_jwt_claims()`
 one possible use case for claims are access level control, which is shown below
 """
-@jwt.user_claims_loader
+@jwt.additional_claims_loader
 def add_claims_to_jwt(identity):
     if identity == 1:   # instead of hard-coding, we should read from a config file to get a list of admins instead
         return {'is_admin': True}
@@ -37,7 +37,7 @@ def add_claims_to_jwt(identity):
 
 
 # This method will check if a token is blacklisted, and will be called automatically when blacklist is enabled
-@jwt.token_in_blacklist_loader
+@jwt.token_in_blocklist_loader
 def check_if_token_in_blacklist(decrypted_token):
     return decrypted_token['jti'] in BLACKLIST
 
